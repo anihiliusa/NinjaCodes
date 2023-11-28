ab577f5a60c4819802dc9b2b01d846fa81167add
AnihiliusA <26289415+anihiliusa@users.noreply.github.com>
2023-11-28 16:40:58 +0200

    Update CryptoNinjaCodes.py

--- CryptoNinjaCodes.py
+++ CryptoNinjaCodes.py
@@ -1,47 +1,71 @@
-	from hummingbot.strategy.infinity_grid import InfinityGridStrategy
- 
-class MyStrategy(InfinityGridStrategy):
-    def init(self, **kwargs):
-        super().init(**kwargs)
- 
-        self.trailing_stop_step_size = 0.001
-        self.trailing_stop_min_distance = 0.005
-        self.grid_size = 0.001
-        self.grid_price_step_size = 0.01
+import logging
+from decimal import Decimal
+from typing import Dict, List
+
+import numpy as np
+import pandas as pd
+
+from hummingbot.connector.connector_base import InfinityGridStrategy
+from hummingbot.core.data_type.common import OrderType, PriceType, TradeType
+from hummingbot.core.data_type.order_candidate import OrderCandidate
+from hummingbot.core.event.events import BuyOrderCompletedEvent, OrderFilledEvent, SellOrderCompletedEvent
+from hummingbot.core.utils import map_df_to_str
+from hummingbot.strategy.infinity_grid import InfinityGridStrategy
+
+
+class CryptoNinjaCodes(InfinityGridStrategy):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.trailing_stop_step_size = Decimal("0.001")
+        self.trailing_stop_min_distance = Decimal("0.005")
+        self.grid_size = Decimal("0.001")
+        self.grid_price_step_size = Decimal("0.01")
         self.trailing_stop_order_type = "limit"
-        self.trailing_stop_order_id = None
- 
+
     def on_order_filled(self, order_filled_event):
+        # Изчисляване на текущата цена за проследяващ стоп
         trailing_stop_price = order_filled_event.price - self.spread
-        if order_filled_event.order_id == self.trailing_stop_order_id:
+
+        # Проверка дали изпълнената поръчка е поръчката, която е задействала защитния стоп, и ако да, дали е от същата страна
+        if order_filled_event.order_id == self.trailing_stop_order_id and order_filled_event.order.side == self.trailing_stop_order_side:
+            # Затваряне на поръчката
             order_filled_event.order.cancel()
         else:
+            # Изчисляване на новата цена за проследяващ стоп
             trailing_stop_price = order_filled_event.price + self.trailing_stop_step_size
+
+            # Създаване на нова поръчка за проследяващ стоп
             self.trailing_stop_order_id = self.exchange.create_limit_order(
                 symbol=self.trading_pair,
                 side=order_filled_event.order.side,
                 amount=order_filled_event.order.amount,
                 price=trailing_stop_price,
             )
- 
+
     def on_data(self, data):
-        trailing_stop_price = self.market_info.price - self.trailing_stop_min_distance
+        # Проверка дали е необходимо да се задейства защитен стоп
         if self.market_info.price < trailing_stop_price:
+            # Затваряне на всички активни поръчки
             for order in self.active_orders:
                 order.cancel()
+
+            # Рестартиране на стратегията
             self.restart()
- 
- 
+
+
 def main():
-    strategy = MyStrategy()
+    strategy = CryptoNinjaCodes()
     strategy.set_symbol("XAUT-USDT")
     strategy.set_exchange("gate_io")
     strategy.set_trading_pair("XAUT/USDT")
-    strategy.set_grid_size(0.001)
-    strategy.set_grid_price_step_size(0.01)
+    strategy.set_grid_size(Decimal("0.001"))
+    strategy.set_grid_price_step_size(Decimal("0.01"))
     strategy.set_trailing_stop_order_type("limit")
     strategy.start()
- 
- 
+
+
 if __name__ == "__main__":
     main()
+
